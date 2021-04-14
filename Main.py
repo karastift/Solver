fedab5d195148e5f620a02281334941c112e4726
karastift <kiansinan@gmail.com>
2021-04-14 09:54:41 +0200

    implemented menu

--- Main.py
+++ Main.py
@@ -1,12 +1,7 @@
-from Solver import Solver
+from Menu import Menu
 
 def main():
-    # create menu
-    S = Solver()
-    convertedImage = S.convertImage('example.png')
-    convertedString = S.convertString(convertedImage)
-    solution = S.calculateSphere(convertedString)
-    S.printSolution(solution)
+    Menu()
 
 if __name__ == '__main__':
     main()
